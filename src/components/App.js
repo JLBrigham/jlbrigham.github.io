f28275b7575ad86741dd065f0842c99cbd9ed2f6
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-08-02 14:17:40 +0200

    Create Photo component, image not yet rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Photo from "./Photo";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header /> <Photo img="../public/pencilfavicon.jpeg" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Photo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Photo(props) {
+  return <img src={props.img} alt="avatar_img" />;
+}
+
+export default Photo;
