7c12abf43426aa1e516abcbc01e0408154f55be2
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-10-29 16:38:39 +0200

    Updated positioning for about section to use flexbox

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,13 @@ import "./About.css";
 function About(props) {
   return (
     <div id="about" className="about">
-      <img className="profile-pic" src={props.img} alt="avatar_img" />
-      <h1 className="my-name">{props.name}</h1>
-      <p className="intro-para">{props.intro}</p>
+      <div className="name-container">
+        <h1 className="my-name">{props.name}</h1>
+      </div>
+      <div className="about-contents">
+        <img className="profile-pic" src={props.img} alt="avatar_img" />
+        <p className="intro-para">{props.intro}</p>
+      </div>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
           img={"/profile-photo.jpg"}
           name={"Joanna Brigham"}
           intro={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat tempor mollis. Curabitur lectus purus, accumsan et justo volutpat, consequat pulvinar neque. Sed fermentum orci consectetur nunc sodales, eget mattis massa eleifend. Nunc sollicitudin leo orci, in egestas nibh posuere ac. Curabitur lobortis quis ex a tempus. Nulla tempus venenatis arcu, vitae vestibulum neque bibendum sit amet. Curabitur dui augue, cursus pellentesque dictum eu, convallis quis urna. Etiam ac lacus in tellus semper dapibus sed a orci. Nam laoreet varius turpis, eget vulputate neque lobortis quis. Morbi vel porta purus."
+            "Hi, I'm a junior full-stack engineer with a passion for front-end development. I'm currently looking for my first role in tech and would love to hear from you. I'm based in Chamonix, France."
           }
         />
         <Skills />
