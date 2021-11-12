434a8524f17d2aef021ca5ff797d438268ca31b0
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-11-12 11:58:45 +0100

    Improved styling of past experiences

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ import "./Experience.css";
 function Experience() {
   return (
     <div className="experience" id="experience">
-      <SectionTitle title={"Experience - Work In Progress . . ."} />
+      <SectionTitle title={"Experience"} />
       <SubTitle
         text={
           "A snap-shot of my previous experience and education and the skills they've given me"
@@ -20,14 +20,14 @@ function Experience() {
               job={"Operations Executive + Airport Manager"}
               company={"Crystal Ski, TUI Group, 2017 - 2020"}
               content={
-                "A can-do attitude, attention to detail, communication, team-work, resillience, calm under pressure."
+                "a can-do attitude • attention to detail • communication • team-work • resillience • calm under pressure"
               }
             />
             <PastExperience
               job={"Bed & Breakfast Sole Host"}
               company={"Alpes Travel, 2018 - Present"}
               content={
-                "Problem solving, customer service, time management and prioritisation, interpersonal skills,"
+                "problem solving • initiative • customer service • time management and prioritisation • interpersonal skills"
               }
             />
           </div>
@@ -36,7 +36,7 @@ function Experience() {
               job={"Property Manager"}
               company={"Foxtons ltd, 2015 - 2017"}
               content={
-                "Logical thinking, mediation, problem solving, working to deadlines, building meaningful client relationships"
+                "logical thinking • mediation • problem solving • working to deadlines • building meaningful client relationships"
               }
             />
           </div>
@@ -46,7 +46,7 @@ function Experience() {
             job={"Makers Academy"}
             company={"16 week intensive coding bootcamp"}
             content={
-              "Combination of self-led learning, workshops and extensive pair programming with peers,Group engineering projects using Agile/XP working practices to work more efficiently. Running stand-ups, retros, sprints, creating MVPs and user stories, Learnt how to follow key coding principles - MVC, OOP, TDD - and practice these on each project. Emphasis on code quality - DRY and SRP. Completely self-led final project, as a group we taught ourselves React Native and MongoDB from scratch and created an app we were proud to present in just 2 weeks"
+              "self-led learning • pair-programming • group engineering projects • agile/XP working practices • running stand-ups, retros, sprints, creating MVPs and user stories • key coding principles - MVC, OOP, TDD • code quality - DRY and SRP • Applied everything learnt in final group project where we learnt the MERN stack from scratch"
             }
           />
         </div>
